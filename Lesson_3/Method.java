cdacbda73424d03433308df07b3beb6b016f53ca
Eprem Chepnyan <156126721+EpChep@users.noreply.github.com>
2024-01-17 23:55:36 +0300

    Commit

--- Lesson_3/Method.java
+++ Lesson_3/Method.java
@@ -1,2 +1,28 @@
+import java.util.Scanner;
 public class Method {
+    public static void main(String[] args) {
+        firstMethod();
+        //checkSumSign();
+        //printColor();
+        //compareNumbers();
+    }
+
+    public static void firstMethod() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите первое целое число: ");
+        int number1 = scanner.nextInt();
+        System.out.println("Введите второе целое число: ");
+        int number2 = scanner.nextInt();
+        boolean result = trueOrFalse(number1, number2);
+        System.out.println(result);
+    }
+        public static boolean trueOrFalse(int number1, int number2) {
+            int sum = number1 + number2;
+            if (sum > 10 && sum <= 20) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
 }
