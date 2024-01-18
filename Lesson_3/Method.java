d1732a36781d09f0465770f497fd5e7e54a78e30
Eprem Chepnyan <156126721+EpChep@users.noreply.github.com>
2024-01-18 22:50:34 +0300

    Commit

--- Lesson_3/Method.java
+++ Lesson_3/Method.java
@@ -2,9 +2,9 @@ import java.util.Scanner;
 public class Method {
     public static void main(String[] args) {
         firstMethod();
-        //checkSumSign();
-        //printColor();
-        //compareNumbers();
+        secondMethod();
+        thirdMethod();
+        fourthMethod();
     }
 
     public static void firstMethod() {
@@ -24,5 +24,34 @@ public class Method {
                 return false;
             }
         }
+    public static void secondMethod() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите положительное или отрицательное число: ");
+        int numeric = scanner.nextInt();
+        if (numeric < 0) {
+            System.out.println("Введенно отрицательное число.");
+        } else {
+            System.out.println("Введенно положительное число.");
+        }
+    }
+    public static void thirdMethod() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите целое число: ");
+        int num = scanner.nextInt();
+        boolean res = minusOrPlus(num);
+        System.out.println(res);
+    }
+    public static boolean minusOrPlus(int num) {
+        return num < 0;
+        }
+    public static void fourthMethod(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите сообщение: ");
+        String string = scanner.nextLine();
+        System.out.println("Введите число: ");
+        int number = scanner.nextInt();
+
+    }
 
 }
+
