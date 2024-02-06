78fe4bde51470411e528191d2d4b27231dbc5382
Eprem Chepnyan <156126721+EpChep@users.noreply.github.com>
2024-02-06 14:44:30 +0300

    Update Method.java

--- Lesson_3/Method.java
+++ Lesson_3/Method.java
@@ -64,7 +64,7 @@ public class Method {
 
     }
     public static boolean isLeapYear(int year) {
-        if (year%400 == 0 || year%4==0 && year%100 != 0){
+        if (year%400 == 0 || (year%4==0 && year%100 != 0)){
             return true;
         } else {
             return false;
